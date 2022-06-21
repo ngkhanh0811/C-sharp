26ac9855fb376fe5d364348bc8491f6ccfe2b4a8
nguyenkhanh08112003 <91303308+nguyenkhanh08112003@users.noreply.github.com>
2022-06-21 11:27:01 +0700

    lab1

---
+++ DataTypes/Information/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+class Information
+{
+    static void Main(string[] args)
+    {
+        string name;
+        string address;
+        string phone;
+        var i1 = new { name = "Khanh", address = "Ha Noi", phone = "0987720543" };
+        Console.WriteLine(i1);
+    }
+}
