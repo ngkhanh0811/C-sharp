01399c7fea96ad11d0d5227854d0a27d9cf6a5d4
nguyenkhanh08112003 <91303308+nguyenkhanh08112003@users.noreply.github.com>
2022-06-23 08:49:45 +0700

    BuildingApplication

---
+++ BuildingApplication/BuildingApplication/cs1.cs
@@ -0,0 +1,6 @@
+﻿using System;
+int num = -4;
+if (num < 0)
+{
+    Console.WriteLine("The num is negative");
+}
---
+++ DataTypes/Ex5/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+class Ex5
+{
+    static void Main(string[] args)
+    {
+        double num=1;
+        int x = Convert.ToInt32(Console.ReadLine());
+        for(int i = 1; i <= x; i++)
+        {
+            Console.WriteLine(num*=i);
+        }
+    }
+}
